7488e4efa6b86704eca26cc500466251b8d87269
onestn <onestone.yang@gmail.com>
2020-11-15 08:12:01 +0900

    2.1.7 프로미스

---
+++ Node.js교과서/2.1.7_프로미스.js
@@ -0,0 +1,19 @@
+const condition = true;
+const promise = new Promise((resolve, reject) => {
+	if(condition) {
+		resolve('성공');
+	} else {
+		reject('실패');
+	}
+});
+
+promise
+	.then((message) => {
+		console.log(message);
+	})
+	.catch((error) => {
+		console.error(error);
+	})
+	.finally(() => {
+		console.log('무조건');
+	});
