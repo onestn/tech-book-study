a5e1340af59164a437c72ed275e158a6e4ef107a
onestn <onestone.yang@gmail.com>
2020-11-14 12:29:21 +0900

    구조분해할당

---
+++ Node.js교과서/2.1.5_구조분해할당.js
@@ -0,0 +1,13 @@
+var candyMachine = {
+	status : {
+		name : 'node',
+		count : 5,
+	},
+	getCandy : function() {
+		this.status.count--;
+		return this.status.count;
+	},
+};
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+
