b0c7707937d8c58b3c40ae61aafeb592c63b830e
onestn <onestone.yang@gmail.com>
2020-11-28 00:42:01 +0900

    AJAX

---
+++ Node.js교과서/2.2.1_AJAX_POST.js
@@ -0,0 +1,12 @@
+(async () => {
+	try {
+		const result = await axios.post('https://www.zerocho.com/api/post/json', {
+			name : 'zerocho',
+			birth : 1994,
+		});
+		console.log(result);
+		console.log(result.data);
+	} catch (error) {
+		console.error(error);
+	}
+})();
