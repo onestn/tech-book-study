aef8c1712919d999a037a66dfd0e9934dc9cf26e
onestn <onestone.yang@gmail.com>
2020-12-31 04:50:49 +0900

    Character

---
+++ swift_Yagom/Chapter3.DataType/3-5.Character.swift
@@ -0,0 +1,12 @@
+let alphabetA: Character = "A"
+print(alphabetA)
+
+// Character 값에 유니코드 문자를 사용할 수 있습니다.
+let commandCharacter: Character = "기호"
+print(commandCharacter)
+
+let 한글변수이름: Character = "ㄱ"
+
+// 한글도 유니코드 문자에 속하므로 스위프트 코드의 변수 이름으로 사용할 수 있습니다.
+print("한글의 첫 자음: \(한글변수이름)")
+
