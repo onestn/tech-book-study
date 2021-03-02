5e1c968559acbe2dab148bc6b7ed5f9754bf6d77
onestn <onestone.yang@gmail.com>
2021-03-02 20:22:51 +0900

    Closures

---
+++ Swift.스위프트 프로그래밍 3판/Chapter15.Map,Reduce/15-2.클로저의_반복_사용.swift
@@ -0,0 +1,9 @@
+let evenNumbers: [Int] = [0, 2, 4, 6, 8]
+let oddNumbers: [Int] = [0, 1, 3, 5, 7]
+let multiplyTwo: (Int) -> Int = { $0 * 2 }
+
+let doubledEvenNumbers = evenNumbers.map(multiplyTwo)
+print(doubledEvenNumbers)
+
+let doubledOddNumbers = oddNumbers.map(multiplyTwo)
+print(doubledOddNumbers)
