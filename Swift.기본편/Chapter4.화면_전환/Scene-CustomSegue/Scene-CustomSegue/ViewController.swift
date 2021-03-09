d95585335ac099750d13f1e8da20079c1f4477da
onestn <onestone.yang@gmail.com>
2021-03-10 08:26:20 +0900

    iOS

--- Swift.기본편/Chapter4.화면_전환/Scene-CustomSegue/Scene-CustomSegue/ViewController.swift
+++ Swift.기본편/Chapter4.화면_전환/Scene-CustomSegue/Scene-CustomSegue/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 class ViewController: UIViewController {
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        NSLog("세그웨이가 곧 실행됩니다.")
+        
+        if (segue.identifier == "custom_segue") {
+            NSLog("커스텀 세그가 실행됨")
+        } else if (segue.identifier == "action_segue") {
+            NSLog("액션 세그가 실행됨")
+        } else {
+            NSLog("알 수 없는 세그입니다.")
+        }
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
 }
-
---
+++ Swift.기본편/Chapter4.화면_전환/SubmitValue/SubmitValue/ResultViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ResultViewController.swift
+//  SubmitValue
+//
+//  Created by 양원석 on 2021/03/09.
+//
+
+import Foundation
