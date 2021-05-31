99af2ea0d8ad7054e5aeeca9b346eaadd2aa9a08
onestn <onestone.yang@gmail.com>
2021-06-01 08:42:48 +0900

    Swift

--- Swift.스위프트 프로그래밍 3판/Chapter13.클로저/13-20.withoutActuallyEscaping_함수의_활용.swift
+++ Swift.스위프트 프로그래밍 3판/Chapter13.클로저/13-20.withoutActuallyEscaping_함수의_활용.swift
@@ -4,15 +4,13 @@ let evenNumberPredicate = { (number: Int) -> Bool in
     return number % 2 == 0
 }
 
-let oddNumberPredicate = { (number: Int) -> Bool in 
+let oddNumberPredicate = { (number: Int) -> Bool in
     return number % 2 == 1
 }
 
 func hasElements(in array: [Int], match predicate: (Int) -> Bool) -> Bool {
-    return withoutActuallyEscaping(predicate, do: { escapablePredicate in
-        return (array.lazy.filter { escapablePredicate($0) }.isEmpty == false)
-                                   })
-    }
+    return withoutActuallyEscaping(predicate, do: { escapablePredicate in return (array.lazy.filter { escapablePredicate($0) }.isEmpty == false) })
+}
 
 let hasEvenNumber = hasElements(in: numbers, match: evenNumberPredicate)
 let hasOddNumber = hasElements(in: numbers, match: oddNumberPredicate)
