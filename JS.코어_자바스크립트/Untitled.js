9d5389d2f519b7862211a2874ef37169c716f53b
onestn <onestone.yang@gmail.com>
2021-06-08 16:12:43 +0900

    MorseSound

---
+++ JS.코어_자바스크립트/Untitled.js
@@ -0,0 +1,140 @@
+var AudioContext = window.AudioContext || window.webkitAudioContext;
+var ctx = new AudioContext();
+var dot = 1.2 / 15;
+
+document.getElementById("demo").onsubmit = function() {
+    var t = ctx.currentTime;
+
+    var oscillator = ctx.createOscillator();
+    oscillator.type = "sine";
+    oscillator.frequency.value = 600;
+
+    var gainNode = ctx.createGain();
+    gainNode.gain.setValueAtTime(0, t);
+
+    this.code.value.split("").forEach(function(letter) {
+        switch(letter) {
+            case ".":
+                gainNode.gain.setValueAtTime(1, t);
+                t += dot;
+                gainNode.gain.setValueAtTime(0, t);
+                t += dot;
+                break;
+            case "-":
+                gainNode.gain.setValueAtTime(1, t);
+                t += 3 * dot;
+                gainNode.gain.setValueAtTime(0, t);
+                t += dot;
+                break;
+            case " ":
+                t += 7 * dot;
+                break;
+        }
+    });
+
+    oscillator.connect(gainNode);
+    gainNode.connect(ctx.destination);
+
+    oscillator.start();
+
+    return false;
+}
+
+
+// Copyright 2014–2017, Eric Holk
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+function MorseNode(ac, rate) {
+    // ac is an audio context.
+    this._oscillator = ac.createOscillator();
+    this._gain = ac.createGain();
+    this._gain.gain.value = 0;
+    this._oscillator.frequency.value = 750;
+    this._oscillator.connect(this._gain);
+    if(rate == undefined)
+        rate = 20;
+    this._dot = 1.2 / rate; // formula from Wikipedia.
+    this._oscillator.start(0);
+}
+MorseNode.prototype.connect = function(target) {
+    return this._gain.connect(target);
+}
+MorseNode.prototype.MORSE = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "0": "-----"
+};
+MorseNode.prototype.playChar = function(t, c) {
+    for(var i = 0; i < c.length; i++) {
+        switch(c[i]) {
+            case '.':
+                this._gain.gain.setValueAtTime(1.0, t);
+                t += this._dot;
+                this._gain.gain.setValueAtTime(0.0, t);
+                break;
+            case '-':
+                this._gain.gain.setValueAtTime(1.0, t);
+                t += 3 * this._dot;
+                this._gain.gain.setValueAtTime(0.0, t);
+                break;
+        }
+        t += this._dot;
+    }
+    return t;
+}
+MorseNode.prototype.playString = function(t, w) {
+    w = w.toUpperCase();
+    for(var i = 0; i < w.length; i++) {
+        if(w[i] == ' ') {
+            t += 3 * this._dot; // 3 dots from before, three here, and
+            // 1 from the ending letter before.
+        }
+        else if(this.MORSE[w[i]] != undefined) {
+            t = this.playChar(t, this.MORSE[w[i]]);
+            t += 2 * this._dot;
+        }
+    }
+    return t;
+}
